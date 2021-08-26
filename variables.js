15509b3c3671b74d43a721a43f8bb7441d5f19b8
sucheta-codekul <75297233+sucheta-codekul@users.noreply.github.com>
2021-08-26 17:57:40 +0530

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,42 @@
+//variable - container in which we can store values
+//ES6
+
+//var,let,const
+//camelCase
+
+//var- allows to redeclare a variable with same name,
+
+//let- does not allow a variable with same name
+//const - does not allow a variable with same name,always initalized(cannot be redeclared or updated)
+
+var x = 100//Initialization,
+var x//Declaration
+x=100//assignment
+
+var numberOne = 50
+var numberTwo = 40
+
+console.log(numberOne+numberTwo)
+
+var id =100
+console.log(id)
+var id=200
+console.log(id)
+
+// let firstName = "ABC"
+// console.log(firstName)
+// let firstName = "XYZ"
+// console.log(firstName)
+// let firstName
+// let firstName='qwerty'
+// console.log(firstName)
+
+
+// const price
+// price=499.99
+// console.log(price)
+
+const price = 300.99
+const price = 299
+console.log(price)
+
